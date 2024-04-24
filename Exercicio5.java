3f5aa94964334ff63ddf2b24181fd33598d32a1a
frostxx78 <113647362+frostxx78@users.noreply.github.com>
2024-04-24 18:17:45 -0300

    Add files via upload

--- Exercicio5.java
+++ Exercicio5.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class Exercicio5 {
-  public static void main(String args[]) {
-   Scanner in = new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
 
-   System.out.print("Digite o salario: ");
-   Double = in.nextDouble();
-   in.nextLine();
-   Double taxa = in.nextDouble();
-   Double resultado = (salario/100)*taxa;
+        System.out.print("Digite o salario: ");
+        double salario = in.nextDouble();
+        in.nextLine(); 
 
-   System.out.println("salario com o percentual: " + resultado);
+        System.out.print("Digite a taxa (em %): ");
+        double taxa = in.nextDouble();
 
-   }
+        double resultado = (salario * taxa) / 100.0;
+
+        System.out.println("Salario com o percentual: " + resultado);
+
+        in.close();
+    }
 }
