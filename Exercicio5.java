ac7a45bcdcb145dcf235153053e2ce63d99d9bdd
frostxx78 <113647362+frostxx78@users.noreply.github.com>
2024-04-23 21:53:24 -0300

    Add files via upload

---
+++ Exercicio5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio5 {
+  public static void main(String args[]) {
+   Scanner in = new Scanner(System.in);
+
+   System.out.print("Digite o salario: ");
+   Double = in.nextDouble();
+   in.nextLine();
+   Double taxa = in.nextDouble();
+   Double resultado = (salario/100)*taxa;
+
+   System.out.println("salario com o percentual: " + resultado);
+
+   }
+}
