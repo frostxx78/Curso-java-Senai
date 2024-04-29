7c2e569b996cbff5051d048ef2dc20332a8306a7
frostxx78 <113647362+frostxx78@users.noreply.github.com>
2024-04-29 19:58:19 -0300

    Add files via upload

---
+++ Exercicio11.java
@@ -0,0 +1,16 @@
+public class Exercicio11 {
+    public static void main(String[] args) {
+  
+   for(int i = 1; i<21;i++) {
+
+    if (i % 2!=0){ 
+
+    System.out.println("Número: " + i);
+
+   }
+
+  }
+
+ }
+
+}
