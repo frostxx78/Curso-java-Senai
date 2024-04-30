0d84442c67754be9e08cbc696db56658653d24da
frostxx78 <113647362+frostxx78@users.noreply.github.com>
2024-04-29 21:18:31 -0300

    Update Exercicio13.java

--- Exercicio13.java
+++ Exercicio13.java
@@ -1,10 +1,10 @@
 public class Exercicio13 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
   
-   for(int i = 11; i<1;i--) {
+   for (int i = 10; i >= 1; i--) {
      System.out.println("Número: " + i);
      Thread.sleep(1500);
-   } 
+} 
 
     System.out.println("LANÇAMENTO");
 
