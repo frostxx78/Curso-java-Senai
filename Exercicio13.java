850cb17029d9a66a65bff3802041526cc347edd9
frostxx78 <113647362+frostxx78@users.noreply.github.com>
2024-04-29 21:05:12 -0300

    Add files via upload

---
+++ Exercicio13.java
@@ -0,0 +1,13 @@
+public class Exercicio13 {
+    public static void main(String[] args) {
+  
+   for(int i = 11; i<1;i--) {
+     System.out.println("Número: " + i);
+     Thread.sleep(1500);
+   } 
+
+    System.out.println("LANÇAMENTO");
+
+ }
+
+}
